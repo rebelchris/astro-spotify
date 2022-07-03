5c78d47fbcbd8763395b79c4d338fc2a31195e22
Chris Bongers <chrisbongers@gmail.com>
2022-07-03 16:46:28 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+    adapter: vercel(),
+});
---
+++ src/lib/api.js
@@ -0,0 +1,37 @@
+import querystring from 'querystring';
+
+const get = (path, headers) =>
+    fetch(path, headers).then((r) => r.status === 200 ? r.json() : JSON.stringify({}));
+
+const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+const client_id = import.meta.env.CLIENT_ID;
+const client_secret = import.meta.env.CLIENT_SECRET;
+const refresh_token = import.meta.env.CLIENT_TOKEN;
+
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+const getAccessToken = async () => {
+    const response = await fetch(TOKEN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            Authorization: `Basic ${basic}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: querystring.stringify({
+            grant_type: 'refresh_token',
+            refresh_token
+        })
+    });
+
+    return await response.json();
+};
+
+export const  getCurrentPlaying = async () => {
+    const {access_token} = await getAccessToken();
+    return get(TOP_TRACKS_ENDPOINT, {
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        }
+    });
+}
