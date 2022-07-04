f5b434d14fd09ad70d16090494ec3b11e9a0f762
Chris Bongers <chrisbongers@gmail.com>
2022-07-04 16:18:16 +0200

    Try wrangler way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import cloudflare from '@astrojs/cloudflare';
+import worker, { cloudflare } from 'astro-service-worker/adapter';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: cloudflare(),
+    adapter: worker(cloudflare)
 });
