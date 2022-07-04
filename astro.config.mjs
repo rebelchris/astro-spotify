878688c7c04d9d957232935c6ad2ec935cf75091
Chris Bongers <chrisbongers@gmail.com>
2022-07-04 16:28:30 +0200

    Back on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel';
+// import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: cloudflare()
+    adapter: vercel()
+    // adapter: cloudflare()
 });
