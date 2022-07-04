2e70b116aa5f55a4557f1cfb740f244e9c562c29
Chris Bongers <chrisbongers@gmail.com>
2022-07-04 16:00:35 +0200

    Trying cloudfllare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: vercel(),
+    adapter: cloudflare(),
 });
